454ba981162c98876ff13311ad09fab6e4704afa
FangLy <thefangsni@gmail.com>
2025-02-27 17:27:08 +0700

    feat: integrate Redis service for cache management in ConfigsService

--- src/configs/configs.service.ts
+++ src/configs/configs.service.ts
@@ -3,14 +3,17 @@ import { PrismaService } from '../prisma/prisma.service';
 import { dateFormat } from '../common/utils';
 import { CreateConfigDto } from './dto/create-config.dto';
 import { UpdateConfigDto } from './dto/update-config.dto';
+import { RedisService } from '../redis/redis.service';
 import { Prisma } from '@prisma/client';
+
 
 @Injectable()
 export class ConfigsService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService, private readonly redis: RedisService) {}
   async create(configDto: CreateConfigDto) {
     configDto.createAt = dateFormat(new Date());
     configDto.updateAt = dateFormat(new Date());
+    await this.redis.del("device");
     return this.prisma.configs.create({ data: configDto as unknown as Prisma.ConfigsCreateInput });
   }
 
@@ -33,10 +36,12 @@ export class ConfigsService {
 
   async update(id: string, configDto: UpdateConfigDto) {
     configDto.updateAt = dateFormat(new Date());
-    return this.prisma.configs.update({ 
+    const result = await this.prisma.configs.update({ 
       where: { sn: id },
       data: configDto as unknown as Prisma.ConfigsUpdateInput
     });
+    await this.redis.del("device");
+    return result;
   }
 
   async remove(id: string) {
