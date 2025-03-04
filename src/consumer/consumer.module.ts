38c0d935f411e7c4aae7770d25dccee0f391b060
FangLy <thefangsni@gmail.com>
2025-03-04 23:17:56 +0700

    feat: add ONLINE_SERVICE integration for RabbitMQ in Consumer module and remove redundant service registration from Device module

--- src/consumer/consumer.module.ts
+++ src/consumer/consumer.module.ts
@@ -3,10 +3,24 @@ import { ConsumerService } from './consumer.service';
 import { ConsumerController } from './consumer.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
-import { DeviceModule } from 'src/device/device.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, DeviceModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    ClientsModule.register([
+      {
+        name: 'ONLINE_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ],
+          queue: 'online_queue',
+          queueOptions: { durable: true }
+        }
+      }
+    ]),
+    PrismaModule
+  ],
   providers: [ConsumerService],
   controllers: [ConsumerController]
 })
