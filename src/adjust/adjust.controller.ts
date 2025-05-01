073d86f9958183fc71a3aaaf95b4254857c5db66
FangLy <thefangsni@gmail.com>
2025-05-02 00:53:57 +0700

    fix: update image version to 1.1.7 in GitHub Actions; add online status endpoint in AdjustController and implement logic in AdjustService

--- src/adjust/adjust.controller.ts
+++ src/adjust/adjust.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Param, Put, Patch, Body } from '@nestjs/common';
+import { Controller, Param, Put, Patch, Body, Get, UseGuards, Query } from '@nestjs/common';
 import { AdjustService } from './adjust.service';
 import { UpdateProbeDto } from '../probe/dto/update-probe.dto';
 import { UpdateConfigDto } from '../configs/dto/update-config.dto';
-import { UpdateDeviceDto } from 'src/device/dto/update-device.dto';
+import { UpdateDeviceDto } from '../device/dto/update-device.dto';
+import { JwtAuthGuard } from '../common/guards';
 
 @Controller()
 export class AdjustController {
   constructor(private readonly adjustService: AdjustService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('online')
+  async onlineStatus(@Query('hospital') hospital: string, @Query('ward') ward: string) {
+    return this.adjustService.getOnlineStatus(hospital, ward);
+  }
 
   @Put('adjust/:id')
   async updateProbe(@Param('id') id: string, @Body() updateConfig: UpdateProbeDto) {
--- src/adjust/adjust.service.ts
+++ src/adjust/adjust.service.ts
@@ -5,10 +5,29 @@ import { dateFormat } from '../common/utils';
 import { RedisService } from '../redis/redis.service';
 import { UpdateConfigDto } from '../configs/dto/update-config.dto';
 import { UpdateDeviceDto } from 'src/device/dto/update-device.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AdjustService {
   constructor(private readonly prisma: PrismaService, private readonly redis: RedisService) {}
+
+  async getOnlineStatus(hospital: string, ward: string) {
+    let whereClause = {
+      AND: [
+        { online: false },
+        { NOT: { hospital: 'HID-DEVELOPMENT' } },
+        { NOT: { hospital: 'HOS-5c4b9f29-2b0f-4e19-9442-22c9a72b19df' } },
+      ]
+    } as Prisma.DevicesWhereInput;
+    if (hospital) whereClause = { ...whereClause, hospital };
+    if (ward) whereClause = { ...whereClause, ward };
+    const result = await this.prisma.devices.findMany({
+      where: whereClause,
+      select: { id: true, name: true, hospitalName: true, wardName: true, updateAt: true },
+      orderBy: { seq: 'asc' }
+    });
+    return result;
+  }
 
   async updateProbe(id: string, probeDto: UpdateProbeDto) {
     probeDto.updateAt = dateFormat(new Date());
