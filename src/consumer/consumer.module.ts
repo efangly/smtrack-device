8ec7e32191ca640d36e3ab964d324cbd2fdd6aab
FangLy <thefangsni@gmail.com>
2025-10-01 16:15:56 +0700

    fix: update image version to 1.2.2-b and remove unused ConfigModule import

--- src/consumer/consumer.module.ts
+++ src/consumer/consumer.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConsumerService } from './consumer.service';
 import { ConsumerController } from './consumer.controller';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 
